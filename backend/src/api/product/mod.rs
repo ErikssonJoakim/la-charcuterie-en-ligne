bfaf342a1ae7808144c689bbc0bb8b15aded51d8
ErikssonJoakim <j.eriksson@okp4.com>
2023-07-03 17:42:29 +0200

    feat(api): add update product count handler

--- backend/src/api/product/mod.rs
+++ backend/src/api/product/mod.rs
@@ -1,8 +1,12 @@
 mod get;
+mod patch;
 
 use get::get_product;
+use patch::update_product_count;
 use actix_web::web::ServiceConfig;
 
 pub fn product_config(cfg: &mut ServiceConfig) {
-    cfg.service(get_product);
+    cfg
+    .service(get_product)
+    .service(update_product_count);
 }
---
+++ backend/src/api/product/patch.rs
@@ -0,0 +1,54 @@
+use crate::db_connection::{Database, DatabaseConnection};
+use actix_web::{web::{Json, Path}, patch, HttpResponse, Responder};
+use serde::{Serialize, Deserialize};
+use mysql::{prelude::Queryable, params, Error, DriverError, Statement, PooledConn};
+
+#[derive(Serialize, Deserialize)]
+pub struct Data {
+    delta: i32
+}
+struct StatementQuery {
+    stmt: Statement,
+    conn: PooledConn
+}
+
+#[patch("/{id}")]
+pub async fn update_product_count(id: Path<u32>, data: Json<Data>) -> impl Responder {
+    let result = Database::connect()
+    .and_then(|mut conn |
+        conn.prep(
+            "UPDATE sales_item 
+            SET quantity = quantity + :delta
+            WHERE id=:id"
+        )
+        .and_then( | stmt |
+            Ok(
+                StatementQuery {
+                stmt,
+                conn
+                }
+            )
+        )
+    )
+    .and_then(|mut query | query.conn.exec_drop(
+        query.stmt, 
+        params! { "id" => id.into_inner(), "delta" => data.delta }
+    ));
+
+    match result {
+        Ok(_) => HttpResponse::Ok(),
+        Err(e) => match e {
+           Error::MySqlError(e) => match e.code {
+                1690 => HttpResponse::Conflict() , // out of range
+                _ => HttpResponse::InternalServerError()
+           },
+           Error::DriverError(e) => match e {
+               DriverError::ConnectTimeout => HttpResponse::GatewayTimeout(),
+               DriverError::CouldNotConnect(None) => HttpResponse::ServiceUnavailable(),
+               _ => HttpResponse::InternalServerError()
+           },
+           Error::FromValueError(_) => HttpResponse::BadRequest(),
+           _ => HttpResponse::InternalServerError()
+       } 
+    }
+}
