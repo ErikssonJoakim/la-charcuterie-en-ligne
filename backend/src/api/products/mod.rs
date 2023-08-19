df289a357702f9457ba5098537dca2b65da30f89
ErikssonJoakim <j.eriksson@okp4.com>
2023-08-19 20:52:33 +0200

    feat(api): align api with rest structure

--- backend/src/api/products/mod.rs
+++ backend/src/api/products/mod.rs
@@ -1,8 +1,12 @@
 mod get;
+mod patch;
 
 use actix_web::web::ServiceConfig;
-use get::get_products;
+use get::{get_product, get_products};
+use patch::update_product_count;
 
 pub fn products_config(cfg: &mut ServiceConfig) {
-    cfg.service(get_products);
+    cfg.service(get_products)
+        .service(get_product)
+        .service(update_product_count);
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,19 +3,15 @@ mod db_connection;
 mod model;
 
 use actix_web::{web, App, HttpServer};
-use api::{product::product_config, products::products_config};
+use api::products::products_config;
 use dotenvy::dotenv;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    HttpServer::new(move || {
-        App::new()
-            .service(web::scope("/product").configure(product_config))
-            .service(web::scope("/products").configure(products_config))
-    })
-    .bind(("127.0.0.1", 8081))?
-    .run()
-    .await
+    HttpServer::new(move || App::new().service(web::scope("/products").configure(products_config)))
+        .bind(("127.0.0.1", 8081))?
+        .run()
+        .await
 }
