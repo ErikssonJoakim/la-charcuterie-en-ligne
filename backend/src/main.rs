38c109e355272677105aa9f4d02db76dab751d46
ErikssonJoakim <j.eriksson@okp4.com>
2023-10-01 12:23:10 +0200

    feat: configure backend server address binding

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,13 +5,16 @@ mod model;
 use actix_web::{web, App, HttpServer};
 use api::products::products_config;
 use dotenvy::dotenv;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    let address = env::var("API_ADDRESS");
+    let port = env::var("API_PORT");
 
     HttpServer::new(move || App::new().service(web::scope("/products").configure(products_config)))
-        .bind(("127.0.0.1", 8081))?
+        .bind(format!("{}:{}", address.unwrap(), port.unwrap()))?
         .run()
         .await
 }
