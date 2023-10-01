971ed52f8e72593d7ecd3084441f677c5083fee6
ErikssonJoakim <j.eriksson@okp4.com>
2023-10-01 12:26:16 +0200

    feat(database): add measurement command sales item and relay point tables

--- database/config/init-db/02-tables.sql
+++ database/config/init-db/02-tables.sql
@@ -6,13 +6,21 @@ CREATE TABLE IF NOT EXISTS label (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS measurement (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(80) NOT NULL,
+    abbreviation VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+ );
+
 CREATE TABLE IF NOT EXISTS product (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    measurement_id INT UNSIGNED NOT NULL,
     name VARCHAR(80) NOT NULL,
     description VARCHAR(255),
     cost DECIMAL(8,2) UNSIGNED NOT NULL,
-    cost_measure VARCHAR(80) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    CONSTRAINT FK__measurement_id__product FOREIGN KEY (measurement_id) REFERENCES measurement(id)
  );
 
 CREATE TABLE IF NOT EXISTS product_label (
@@ -45,23 +53,37 @@ CREATE TABLE IF NOT EXISTS sales_item_product (
     CONSTRAINT FK__sales_item_id__sales_item_product FOREIGN KEY (sales_item_id) REFERENCES sales_item(id) ON DELETE CASCADE
  );
 
- CREATE TABLE IF NOT EXISTS client (
+ CREATE TABLE IF NOT EXISTS relay_point (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR(80) NOT NULL,
-    email VARCHAR(80) NOT NULL,
+    address VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
  );
 
  CREATE TABLE IF NOT EXISTS command (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    sales_item_id INT UNSIGNED NOT NULL,
     client_id INT UNSIGNED NOT NULL,
-    quantity INT UNSIGNED NOT NULL,
-    delivery_address VARCHAR(255) NOT NULL,
+    relay_point_id INT UNSIGNED NOT NULL,
     delivery_date DATE NOT NULL,
     created_on DATETIME NOT NULL DEFAULT NOW(),
     updated_on DATETIME ON UPDATE NOW(),
     PRIMARY KEY (id),
-    CONSTRAINT FK__client_id__order FOREIGN KEY (client_id) REFERENCES client(id),
-    CONSTRAINT FK__sales_item_id__order FOREIGN KEY (sales_item_id) REFERENCES sales_item(id)
+    CONSTRAINT FK__client_id__command FOREIGN KEY (client_id) REFERENCES client(id),
+    CONSTRAINT FK__relay_point_id__command FOREIGN KEY (relay_point_id) REFERENCES relay_point(id),
+ );
+
+ CREATE TABLE IF NOT EXISTS command_sales_item (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    sales_item_id INT UNSIGNED NOT NULL,
+    command_id INT UNSIGNED NOT NULL,
+    quantity INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT FK__sales_item_id__command_sales_item FOREIGN KEY (sales_item_id) REFERENCES sales_item(id),
+    CONSTRAINT FK__command_id__command_sales_item FOREIGN KEY (command_id) REFERENCES command(id)
+ );
+
+ CREATE TABLE IF NOT EXISTS client (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(80) NOT NULL,
+    email VARCHAR(80) NOT NULL,
+    PRIMARY KEY (id)
  );
